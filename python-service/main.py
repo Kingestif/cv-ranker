201e11f944b0cb8aa2f8c890cf6d98b7ae33a292
Kingestif <kingestiff@gmail.com>
2025-08-02 22:49:38 +0300

    feat: add sentence-transformer to turn text into embeddings

--- python-service/main.py
+++ python-service/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, File, UploadFile, Form
 from typing import List
 from pdfminer.high_level import extract_text
+from sentence_transformers import SentenceTransformer
 import io
 import docx
 import spacy
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 nlp = spacy.load("en_core_web_sm")
 app = FastAPI()
@@ -24,6 +27,8 @@ def extract_name(text: str) -> str:
             return ent.text
     return "Unknown"
 
+def get_embedding(text):
+    return model.encode(text)
 
 @app.post("/upload")
 async def upload(
@@ -31,6 +36,8 @@ async def upload(
     job_description: str = Form(...)
 ):
     extracted_resumes = []
+    job_embedding = get_embedding(job_description)
+
 
     for file in resumes:
         contents = await file.read()
@@ -44,6 +51,8 @@ async def upload(
 
         # Extract name using NER
         name = extract_name(text)
+
+        resume_embedding = get_embedding(text)
 
         extracted_resumes.append({
             "filename": file.filename,
