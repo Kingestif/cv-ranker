15031d3ccc9f820fa4402d38397e24a0a34907b2
Kingestif <kingestiff@gmail.com>
2025-08-04 23:32:21 +0300

    refactor: combined job description score and query score into one final score for similarity search

--- python-service/search.py
+++ python-service/search.py
@@ -1,11 +1,13 @@
 from redis_client import get_session
 import numpy as np
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter, HTTPException, status
 import faiss
 from sentence_transformers import SentenceTransformer
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
 router = APIRouter()
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
@@ -15,41 +17,61 @@ class SearchRequest(BaseModel):
 
 @router.post("/search")
 async def search_resumes(req: SearchRequest):
+    alpha_jd = float(os.getenv("ALPHA_JD", 0.7))
+    alpha_query = float(os.getenv("ALPHA_QUERY", 0.3))
+    
     session_id = req.session_id
     query = req.query
 
     session = get_session(session_id)
     if not session:
-        return {"error": "Session expired or not found"}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Session expired or not found"
+        )
 
     resumes = session["resumes"]
     if not resumes:
-        return {"error": "No resumes found in session"}
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="No resumes found in session"
+        )
 
     # resume embeddings
     resume_embeddings = np.array([r["embedding"] for r in resumes], dtype='float32')
+    faiss.normalize_L2(resume_embeddings)
 
     # query embedding
     query_embedding = model.encode([query], convert_to_numpy=True)
     faiss.normalize_L2(query_embedding)
 
+    jd_scores = [r["similarity"] for r in resumes]  # JD-based similarity
+
     # Search top matches
     d = resume_embeddings.shape[1]
     index = faiss.IndexFlatIP(d)
     index.add(resume_embeddings)
-    k = min(5, len(resumes))
-    
-    distances, indices = index.search(query_embedding, k)
+    k = len(resumes)
+    query_scores, indices = index.search(query_embedding, k)
 
     top_matches = []
     for i, idx in enumerate(indices[0]):
+        jd_score = jd_scores[idx]
+        query_score = float(query_scores[0][i])
+        final_score = alpha_jd * jd_score + 0.3 * alpha_query
+
         r = resumes[idx]
+        
         top_matches.append({
             "filename": r["filename"],
             "applicant_name": r["applicant_name"],
-            "similarity": round(float(distances[0][i]), 4),
-            "text_preview": r["text_preview"]  
+            "text_preview": r["text_preview"],
+            "jd_score": round(jd_score, 4),
+            "query_score": round(query_score, 4),
+            "final_score": round(final_score, 4)
         })
+
+    top_matches.sort(key=lambda x: x["final_score"], reverse=True)
 
     return {
         "query": query,
