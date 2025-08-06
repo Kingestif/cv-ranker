f091d72d1bafa4b9b7e3746e34988260ae7bde64
Kingestif <kingestiff@gmail.com>
2025-08-07 00:28:20 +0300

    feat: enable CORS for API routes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import uploadRoute  from "./routes/uploadRoute";
 import searchRoute from "./routes/searchRoute";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true,               
+}));
+
 app.use('/api/v1/upload', uploadRoute);
 app.use('/api/v1/search', searchRoute);
 
